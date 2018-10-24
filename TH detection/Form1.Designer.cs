867448080575a556245b33a553231bdd43de3893
ChristinAdol <christinadol.wrh@gmail.com>
2018-10-24 17:13:34 +0800

    origin version

--- TH detection/Form1.Designer.cs
+++ TH detection/Form1.Designer.cs
@@ -29,12 +29,138 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.panel4 = new System.Windows.Forms.Panel();
+			this.panel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			this.panel3.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(584, 58);
+			this.panel1.TabIndex = 0;
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 1000;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// panel2
+			// 
+			this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.panel2.Controls.Add(this.chart1);
+			this.panel2.Location = new System.Drawing.Point(12, 64);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(560, 268);
+			this.panel2.TabIndex = 1;
+			// 
+			// chart1
+			// 
+			chartArea1.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea1);
+			this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+			legend1.Name = "Legend1";
+			this.chart1.Legends.Add(legend1);
+			this.chart1.Location = new System.Drawing.Point(0, 0);
+			this.chart1.Name = "chart1";
+			series1.ChartArea = "ChartArea1";
+			series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+			series1.Legend = "Legend1";
+			series1.Name = "Series1";
+			series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Time;
+			this.chart1.Series.Add(series1);
+			this.chart1.Size = new System.Drawing.Size(556, 264);
+			this.chart1.TabIndex = 0;
+			this.chart1.Text = "chart1";
+			// 
+			// panel3
+			// 
+			this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.panel3.Controls.Add(this.chart2);
+			this.panel3.Location = new System.Drawing.Point(12, 338);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(560, 268);
+			this.panel3.TabIndex = 2;
+			// 
+			// chart2
+			// 
+			chartArea2.Name = "ChartArea1";
+			this.chart2.ChartAreas.Add(chartArea2);
+			this.chart2.Dock = System.Windows.Forms.DockStyle.Fill;
+			legend2.Name = "Legend1";
+			this.chart2.Legends.Add(legend2);
+			this.chart2.Location = new System.Drawing.Point(0, 0);
+			this.chart2.Name = "chart2";
+			series2.ChartArea = "ChartArea1";
+			series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+			series2.Legend = "Legend1";
+			series2.Name = "Series1";
+			series2.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Time;
+			this.chart2.Series.Add(series2);
+			this.chart2.Size = new System.Drawing.Size(556, 264);
+			this.chart2.TabIndex = 0;
+			this.chart2.Text = "chart2";
+			// 
+			// panel4
+			// 
+			this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panel4.Location = new System.Drawing.Point(0, 611);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(584, 30);
+			this.panel4.TabIndex = 3;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(584, 641);
+			this.Controls.Add(this.panel3);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.panel4);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			this.panel3.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
 	}
 }
 
--- TH detection/Form1.cs
+++ TH detection/Form1.cs
@@ -7,14 +7,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace TH_detection
 {
 	public partial class Form1 : Form
 	{
+		private DateTime minTime, maxTime;
+
+		Random rnd = new Random();      //随机数
+
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		/*******************************************************************************
+		* 函数名		：AddNewPoint()
+		* 函数功能		：为chart添加新的点
+		* 输入			：timeStamp、ptSeries
+		* 输出			：无
+		* 作者			：ChristinAdol
+		*******************************************************************************/
+		private void AddNewPoint(DateTime timeStamp, Series ptSeries)
+		{
+			//将新数据点添加到其系列中
+			ptSeries.Points.AddXY(timeStamp.ToOADate(), rnd.Next(500));
+
+			//删除超过当前时间4min前的点。
+			double removePoint = timeStamp.AddMinutes((double)(4) * (-1)).ToOADate();      //设定当前时间4min前的点
+			while (ptSeries.Points[0].XValue < removePoint)
+			{
+				ptSeries.Points.RemoveAt(0);    //将点移除
+			}
+			chart1.ChartAreas[0].AxisX.Minimum = ptSeries.Points[0].XValue;     //x轴时间最小值
+			chart1.ChartAreas[0].AxisX.Maximum = DateTime.FromOADate(ptSeries.Points[0].XValue).AddMinutes(5).ToOADate();    //x轴时间最大值
+
+			chart2.ChartAreas[0].AxisX.Minimum = ptSeries.Points[0].XValue;     //x轴时间最小值
+			chart2.ChartAreas[0].AxisX.Maximum = DateTime.FromOADate(ptSeries.Points[0].XValue).AddMinutes(5).ToOADate();    //x轴时间最大值
+
+			chart1.Invalidate();
+			chart2.Invalidate();
+		}
+
+		/*******************************************************************************
+		* 函数名		：timer1_Tick
+		* 函数功能		：定时函数
+		* 输入			：无
+		* 输出			：无
+		* 作者			：ChristinAdol
+		*******************************************************************************/
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			timer1.Interval = 1000;     //定时1000ms
+			DateTime timeStamp = DateTime.Now;      //获取当前时间到timeStamp
+			AddNewPoint(timeStamp, chart1.Series[0]);
+			AddNewPoint(timeStamp, chart2.Series[0]);
 		}
 	}
 }
